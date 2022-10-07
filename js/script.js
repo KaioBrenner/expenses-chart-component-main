669781eda1a8185eabaf8ac18a94b6839421451c
KaioBrenner <kaiobrenner460@protonmail.com>
2022-10-07 10:31:56 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+/*mouseover e mouseout do amount*/
+
+function showAmount1(){
+    let txtAmount = document.getElementsByClassName("a1")[0];
+    txtAmount.style.display = "block";
+}
+function hideAmount1(){
+    let txtAmount = document.getElementsByClassName("a1")[0];
+    txtAmount.style.display = "none";
+}
+function showAmount2(){
+    let txtAmount = document.getElementsByClassName("a2")[0];
+    txtAmount.style.display = "block";
+}
+function hideAmount2(){
+    let txtAmount = document.getElementsByClassName("a2")[0];
+    txtAmount.style.display = "none";
+}
+
+function showAmount3(){
+    let txtAmount = document.getElementsByClassName("a3")[0];
+    txtAmount.style.display = "block";
+}
+function hideAmount3(){
+    let txtAmount = document.getElementsByClassName("a3")[0];
+    txtAmount.style.display = "none";
+}
+
+function showAmount4(){
+    let txtAmount = document.getElementsByClassName("a4")[0];
+    txtAmount.style.display = "block";
+}
+function hideAmount4(){
+    let txtAmount = document.getElementsByClassName("a4")[0];
+    txtAmount.style.display = "none";
+}
+
+function showAmount5(){
+    let txtAmount = document.getElementsByClassName("a5")[0];
+    txtAmount.style.display = "block";
+}
+function hideAmount5(){
+    let txtAmount = document.getElementsByClassName("a5")[0];
+    txtAmount.style.display = "none";
+}
+
+function showAmount6(){
+    let txtAmount = document.getElementsByClassName("a6")[0];
+    txtAmount.style.display = "block";
+}
+function hideAmount6(){
+    let txtAmount = document.getElementsByClassName("a6")[0];
+    txtAmount.style.display = "none";
+}
+
+function showAmount7(){
+    let txtAmount = document.getElementsByClassName("a7")[0];
+    txtAmount.style.display = "block";
+}
+function hideAmount7(){
+    let txtAmount = document.getElementsByClassName("a7")[0];
+    txtAmount.style.display = "none";
+}
+
+
+
